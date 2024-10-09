374d4e16490a7cf2a07087e490788a769b23bafb
Samuel <signfforce@gmail.com>
2024-10-10 00:43:18 +0300

    initial commit

---
+++ test1/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    <button className="btn">Button</button>
+<button className="btn btn-neutral">Neutral</button>
+<button className="btn btn-primary">Primary</button>
+<button className="btn btn-secondary">Secondary</button>
+<button className="btn btn-accent">Accent</button>
+<button className="btn btn-ghost">Ghost</button>
+<button className="btn btn-link">Link</button>
+    </>
+  )
+}
+
+export default App
